3db20c5a7ff0b2217c9b8765c766dd9e3219f2e9
xiu-du-du <cnimmor@gmail.com>
2021-10-30 19:39:24 +0800

    ts

--- 04/a4_types.ts
+++ 04/a4_types.ts
@@ -1,15 +1,36 @@
 /*
  * @Author: xiuxiu
  * @Date: 2021-10-30 17:45:32
- * @LastEditTime: 2021-10-30 17:53:55
+ * @LastEditTime: 2021-10-30 19:39:20
  * @FilePath: \ts\04\a4_types.ts
  */
-let a1={};
-a1=function () {
-    
+//对象
+let b1:{name:string,[propName:string]:unknown}//表示任意类型的属性
+b1={name:'123',userName:'22',postName:'669'}
+
+//函数
+let c1:(a:number,b:number)=>number
+c1=function (a,b) {
+    return a+b
 }
 
+let d1:number[]
+let e1:Array<number>
+e1=[1,2,3,4,5]
 
-let b1:{name:string,age?:number}
-b1={name:'123',age:22}
+//turple 元祖，元祖就是固定长度的数组
+let h:[string,string]
+h=['hello','world']
 
+//enum 枚举，
+enum Gender{
+    Male=0,
+    Female=1
+}
+let i:{name:string,gender:Gender}
+i={
+    name:"tom",
+    gender:Gender.Male
+}
+
+i.gender===Gender.Male
