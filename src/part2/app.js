4044ce46a16e1be59b2152e56e51f94960000c26
xiu-du-du <cnimmor@gmail.com>
2021-11-02 21:16:34 +0800

    ts

--- src/part2/app.js
+++ src/part2/app.js
@@ -1,7 +1,9 @@
 /*
  * @Author: xiuxiu
  * @Date: 2021-11-01 17:09:20
- * @LastEditTime: 2021-11-01 17:18:09
- * @FilePath: \ts\part2\app.ts
+ * @LastEditTime: 2021-11-02 21:15:09
+ * @FilePath: \ts\src\part2\app.js
  */
 console.log('hello world');
+var num = 10;
+var num1 = 10;
--- src/part2/app.ts
+++ src/part2/app.ts
@@ -1,8 +1,10 @@
 /*
  * @Author: xiuxiu
  * @Date: 2021-11-01 17:09:20
- * @LastEditTime: 2021-11-01 17:18:09
- * @FilePath: \ts\part2\app.ts
+ * @LastEditTime: 2021-11-02 21:09:24
+ * @FilePath: \ts\src\part2\app.ts
  */
 console.log('hello world');
 
+let num=10
+let num1=10
